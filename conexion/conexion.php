20e63fc8ae4150a2d6bd42e34bf1dec2c1398515
srpadilla <srpadilla@misena.edu.co>
2021-11-24 18:55:05 -0500

    con base de datos remota

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,8 +1,9 @@
 <?php 
 class Conexion{    
-    private $dsn='mysql:host=localhost;dbname=adsi';
-    private $usr='root';
-    private $psw='';  
+    //private $dsn='mysql:host=sql437.main-hosting.eu ;dbname=u991668360_2142345';
+    private $dsn='mysql:host=sql437.main-hosting.eu;port=3306 ;dbname=u991668360_2142345';
+    private $usr='u991668360_adsi';
+    private $psw='Adsi2142345';  
     private $cnx;
 public function __construct(){
     try {
