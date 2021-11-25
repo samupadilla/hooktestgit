022eb9a1f642d94f41561363948a75718df3eede
srpadilla <srpadilla@misena.edu.co>
2021-11-24 19:32:24 -0500

    cierre ci-cd

--- controlador/controladorlistar.php
+++ controlador/controladorlistar.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/estilos.css">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../mystyle/estilos.css">
+    <link rel="stylesheet" href="../mystyle/stylesam.css">
     <title>Document</title>
 </head>
 <body>
